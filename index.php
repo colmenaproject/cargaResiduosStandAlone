161b81fa65b392458d6e2c7f8dd66da07c195373
proyect0colmena <proyectocolmena16@gmail.com>
2017-08-09 16:29:55 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulario de Carga de Residuos StandAlone</title>
+</head>
+<body>
+<form action="" method="POST">
+	<select><!--TODO Traer los options desde la tabla residuo_capacidad -->
+		<option value="0.3">0.3 litros</option>
+		<option value="0.5">0.5 litros</option>
+		<option value="0.75">0.5 litros</option>
+		<option value="0.8">0.5 litros</option>
+		<option value="1">0.5 litros</option>
+		<option value="1.25">0.5 litros</option>
+		<option value="1.35">0.5 litros</option>
+		<option value="1.50">0.5 litros</option>	
+		<option value="1.65">0.5 litros</option>
+		<option value="2">2 litros</option>
+		<option value="2.25">2.25 litros</option>
+		<option value="2.5">2.5 litros</option>
+		<option value="3">3 litros</option>
+		<option value="5">5 litros</option>
+		<option value="6">6 litros</option>
+		<option value="Otra">Otra: Indicar</option>
+	</select>
+	<input type="range"/>
+	<button>+</button>
+</form>
+</body>
+</html>
