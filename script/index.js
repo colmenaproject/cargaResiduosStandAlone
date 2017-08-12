5092288dbd61f3d733484bf2d75dee2a4d02a64d
Dipiert <damian_rott2@hotmail.com>
2017-08-12 16:32:56 +0200

    Use of jQuery instead plain JS

--- script/index.js
+++ script/index.js
@@ -1,21 +1,31 @@
-function updateRange(id) {
-   var value = document.getElementById(id).value; 
-   console.log(value);
-}
+$(document).ready(function(){
+  appendRanges();
+});
+
 
 function appendRanges() {
    for(var numRange = 0; numRange < 13; numRange++){//TODO: ver como traer la cantidad de la base
       var newDiv = createDiv();
+      var newLabel = createLabel();
       var newRange = createRange(numRange);
       var newTextField = createTextField(numRange);
+      newDiv.appendChild(newLabel);
       newDiv.appendChild(newRange);
       newDiv.appendChild(newTextField);
       document.getElementsByTagName('form')[0].appendChild(newDiv);
    }   
 }
 
-function createDiv() {	
-	return document.createElement('div');
+function createLabel(){
+  var newLabel = document.createElement('label');
+  newLabel.innerHTML = "Capacidad ()";
+  return newLabel;
+  //TODO: ver como traer las capacidades de la DB
+  //0.3, 0.5, 0.75, 0.8, 0.9, 1, 1.25, 1.35, 1.5, 1.65, 2, 2.25, 2.5, 3, 5, 6, Otra
+}
+
+function createDiv() {  
+  return document.createElement('div');
 }
 
 function createRange(numRange) {
@@ -25,16 +35,21 @@ function createRange(numRange) {
       newRange.min = 0;
       newRange.max = 150;
       newRange.value = 0;
-      newRange.oninput = 'updateRange(' + newRange.id + ')';
-   	  return newRange;
+      $(document).on('input', `#${newRange.id}`, updateRange);
+      return newRange;
+}
+
+function updateRange(event) {
+   var txtField = document.getElementById(event.target.id.replace('range', 'textField'));
+   txtField.value = event.target.value; 
 }
 
 function createTextField(numTextField) {
     var newTextField = document.createElement('input');
     newTextField.id = 'textField_' + ++numTextField;
-    newTextField.type = 'text';
+    newTextField.type = 'text';    
+    newTextField.value = 0;
     newTextField.maxLength = 3; //TODO: traer longitud de max
     newTextField.size = 3;
     return newTextField;
-
 }
