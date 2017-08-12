5fe64177e483b633cb66509fec5595e38536e1c4
Dipiert <damian_rott2@hotmail.com>
2017-08-12 16:32:31 +0200

    Use of jQuery instead plain JS

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
       <meta name="description" content="Formulario de Carga de Residuos StandAlone<">
       <meta name="keywords" content="Proyecto Colmena">
       <meta name="author" content="Damián Rotta">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       <script src="script/index.js"></script>
       <link rel="stylesheet" type="text/css" href="css/styles.css">
-      </head>
+   </head>
    <body>
       <form action="" method="POST">
-         <script>appendRanges();</script>
       </form>
    </body>
 </html>
